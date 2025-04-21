77460636ca7dc548bd6a7c5350dee3141044abbd
Justin <joegana@126.com>
2025-04-21 17:39:31 +0800

    aarUpload.bat

---
+++ aarUpload.bat
@@ -0,0 +1,12 @@
+rem 运行此脚本之前,请把sdk/linker/build.gradle 中的curFlavor 定义注释掉
+cls
+echo "Upload aar  work!"
+echo
+echo
+REM from command: --static-backup
+cmd /c gradlew  "frameanimation:artifactoryPublish"
+cmd /c gradlew  "apng:artifactoryPublish"
+cmd /c gradlew  "awebp:artifactoryPublish"
+cmd /c gradlew  "gif:artifactoryPublish"
+cmd /c gradlew  "awebpencoder:artifactoryPublish"
+cmd /c gradlew  "plugin_glide:artifactoryPublish"
