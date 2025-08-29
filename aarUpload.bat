ddbc402aef9bb1a2409f1001e61519957cf7f198
Justin <joegana@126.com>
2025-08-29 16:13:01 +0800

    aarUpload.bat maintain ，2.8.3.2 version release

--- aarUpload.bat
+++ aarUpload.bat
@@ -4,6 +4,8 @@ echo "Upload aar  work!"
 echo
 echo
 REM from command: --static-backup
+set JAVA_HOME=%JAVA17_HOME%
+set PATH=%JAVA_HOME%/bin;%PATH%
 cmd /c gradlew  "frameanimation:artifactoryPublish"
 cmd /c gradlew  "apng:artifactoryPublish"
 cmd /c gradlew  "awebp:artifactoryPublish"
